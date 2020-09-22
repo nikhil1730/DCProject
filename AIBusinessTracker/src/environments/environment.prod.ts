98eea027f1de7faa481263a7d022801492288866
nikhil1730 <nikhil.chavan1731@gmail.com>
2020-09-21 20:06:07 -0400

    Firebase

--- AIBusinessTracker/src/environments/environment.prod.ts
+++ AIBusinessTracker/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4Y2uIdQjbWS6BqN0Q-Pf95tfK8ZuMDKM",
+    authDomain: "object-detection-a3596.firebaseapp.com",
+    databaseURL: "https://object-detection-a3596.firebaseio.com",
+    projectId: "object-detection-a3596",
+    storageBucket: "object-detection-a3596.appspot.com",
+    messagingSenderId: "739446776852",
+    appId: "1:739446776852:web:8957966339b0cae898f301",
+    measurementId: "G-KW573C6VWB"
+  }
 };
